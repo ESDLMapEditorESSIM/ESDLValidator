615b90a2240e1b0b11edaadef1f9e9f408f1ba74
Anne Blankert <anne.blankert@geodan.nl>
2020-12-01 15:34:00 +0100

    add some more tests to main.py

--- main.py
+++ main.py
@@ -24,14 +24,51 @@ def print_test_esdl(resource):
     # loop all objects in loaded ESDL
     for uuid in resource.uuid_dict:
         esdlObject = resource.uuid_dict[uuid]
-        if isinstance(esdlObject, utils.get_esdl_class_from_string("HeatingDemand")):
+        if issubclass(type(esdlObject), utils.get_esdl_class_from_string("HeatingDemand")):
             print("HeatingDemand found: " + getattr(esdlObject, "name"))
             ports = getattr(esdlObject, 'port')
 
             for port in ports.items:
+                name = getattr (port, "name")
+                id = getattr(port, "id")
                 connectedPorts = getattr(port, "connectedTo")
                 for connectedPort in connectedPorts:
-                    print("port connected to: " + getattr(connectedPort, "id"))
+                    print("port {0} {1} connected to: {2}".format(name, id, getattr(connectedPort, "id")))
+        elif issubclass(type(esdlObject), utils.get_esdl_class_from_string("GenericProducer")):
+            power = getattr(esdlObject, 'power')
+            costinfo = getattr(esdlObject, 'costInformation')
+            if costinfo:
+                marginalcosts = getattr(costinfo, 'marginalCosts')
+                if marginalcosts:
+                    marginalcosts = getattr(marginalcosts, 'value')
+                else:
+                    marginalcosts = 0
+            else:
+                marginalcosts = 0
+            ports = getattr(esdlObject, 'port')
+            hasProfile = 0
+            for port in ports.items:
+                name = getattr(port, "name")
+                id = getattr(port, "id")
+                profile = getattr(port, "profile")
+                if name == 'Out':
+                    hasProfile = len(profile.items)
+            print("producer, power: {0}, marginalcosts: {1}, profile: {2}".format(power, marginalcosts,hasProfile))
+        elif issubclass(type(esdlObject), utils.get_esdl_class_from_string("Conversion")):
+            power = getattr(esdlObject, 'power')
+            hasControlStrategy = False
+            controlStrategy = getattr(esdlObject, 'controlStrategy')
+            if controlStrategy:
+                hasControlStrategy = True
+            ports = getattr(esdlObject, 'port')
+            portcount = len(ports)
+            carriercount = 0
+            for port in ports.items:
+                carrier = getattr(port, 'carrier')
+                if (carrier):
+                    carriercount += 1
+            print("Conversion, power: {0}, hasControlStrategy: {1}, portcount == carriercount: {2}".format(power, hasControlStrategy, (portcount == carriercount)))
+
 
 
 esh = load_test_esdl()
