88af7f611070b63f20a795b9897b1fa1322f3551
time <tim.ebben@geodan.nl>
2021-01-20 13:03:34 +0100

    cleanup test_validator.py, added test multiple schemas at once

--- esdlvalidator/validation/tests/__init__.py
+++ esdlvalidator/validation/tests/__init__.py
@@ -1,3 +1,29 @@
 
+from esdlvalidator.core.esdl import utils
+from esdlvalidator.validation.repository import SchemaRepository
+
 with open('./testdata/validation_test.db', 'w') as file:
     file.write("")
+
+
+def get_test_schema_data(file):
+    with open(file, "r") as file:
+        schemaData = file.read()
+
+    return schemaData
+
+
+def get_test_schema_id(schemaData):
+    repo = SchemaRepository("./testdata/validation_test.db")
+    schemaID = repo.insert(schemaData)
+    return repo.get_by_id(schemaID)
+
+
+def get_test_dataset_ameland():
+    esh = utils.get_esh_from_file("testdata/Ameland_energie_2015.esdl")
+    return esh.resource
+
+
+def get_test_dataset_hybrid():
+    esh = utils.get_esh_from_file("testdata/hybrid_hp_with_pv_storage.esdl")
+    return esh.resource
--- esdlvalidator/validation/tests/test_validator.py
+++ esdlvalidator/validation/tests/test_validator.py
@@ -1,26 +1,29 @@
 import unittest
 
-from esdlvalidator.core.esdl import utils
-
+from esdlvalidator.validation.tests import get_test_schema_data, get_test_schema_id, get_test_dataset_ameland, get_test_dataset_hybrid
 from esdlvalidator.validation.validator import EsdlValidator
-from esdlvalidator.validation.repository import SchemaRepository
 
 
 class TestValidator(unittest.TestCase):
     """Tests for the validator"""
 
+    @classmethod
+    def setUpClass(cls):
+        super(TestValidator, cls).setUpClass()
+        cls.schemaOne = get_test_schema_id(get_test_schema_data("testdata/schema_test_1.json"))
+        cls.schemaTwo = get_test_schema_id(get_test_schema_data("testdata/schema_test_2.json"))
+        cls.esdlAmeland = get_test_dataset_ameland()
+        cls.esdlHybrid = get_test_dataset_hybrid()
+
     def test_validate_schema_1(self):
         """test running the validator"""
 
         # prepare
-        schema = self.get_test_schema_id(self.get_test_schema_data("testdata/schema_test_1.json"))
-        esdl = self.get_test_dataset_ameland()
         validator = EsdlValidator()
 
-        # validate against 1 schema
-        result = validator.validate(esdl, [schema])
-        schemaResult = result.schemas[0]
-        validation = schemaResult.validations[0]
+        # execute, validate against 1 schema
+        result = validator.validate(self.esdlAmeland, [self.schemaOne])
+        validation = result.schemas[0].validations[0]
 
         self.assertEqual(validation.checked, 8, "there should be 8 checked")
         self.assertEqual(len(validation.warnings), 1, "there should be 1 warning")
@@ -30,34 +33,25 @@ class TestValidator(unittest.TestCase):
         """test running the validator on test schema 2 with and, or defined"""
 
         # prepare
-        schema = self.get_test_schema_id(self.get_test_schema_data("testdata/schema_test_2.json"))
-        esdl = self.get_test_dataset_hybrid()
         validator = EsdlValidator()
 
-        # validate against 1 schema
-        result = validator.validate(esdl, [schema])
-        schemaResult = result.schemas[0]
-        validation = schemaResult.validations[0]
+        # execute, validate against 1 schema
+        result = validator.validate(self.esdlHybrid, [self.schemaTwo])
+        validation = result.schemas[0].validations[0]
 
+        # assert
         self.assertEqual(validation.checked, 3, "there should be 3 checked since there are only 3 producers")
         self.assertEqual(len(validation.errors), 2, "there should be 2 errors since 1 producer validates ok")
         self.assertEqual(validation.errors[0], "Consumer missing power and marginal costs or no energy profile connected: None", "Warning should say: Consumer missing power and marginal costs or no energy profile connected: None")
 
-    def get_test_schema_data(self, file):
-        with open(file, "r") as file:
-            schemaData = file.read()
+    def test_validate_multiple_schemas(self):
+        """Test if the validator works with checking multiple schemas"""
 
-        return schemaData
+        # prepare
+        validator = EsdlValidator()
 
-    def get_test_schema_id(self, schemaData):
-        repo = SchemaRepository("./testdata/validation_test.db")
-        schemaID = repo.insert(schemaData)
-        return repo.get_by_id(schemaID)
+        # execute, validate against 2 schemas
+        result = validator.validate(self.esdlHybrid, [self.schemaOne, self.schemaTwo])
 
-    def get_test_dataset_ameland(self):
-        esh = utils.get_esh_from_file("testdata/Ameland_energie_2015.esdl")
-        return esh.resource
-
-    def get_test_dataset_hybrid(self):
-        esh = utils.get_esh_from_file("testdata/hybrid_hp_with_pv_storage.esdl")
-        return esh.resource
+        # assert
+        self.assertEqual(len(result.schemas), 2, "there should be 2 schemas in the result")
