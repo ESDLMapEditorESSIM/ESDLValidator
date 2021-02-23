31ec8e8898b7babca6079ddf6bba72aa8896ebd3
Arun Subramanian <arun.subramanian@tno.nl>
2021-02-23 18:21:13 +0100

    Added the ability for check_not_null function to return message as JSON.

--- esdlvalidator/validation/functions/check_not_null.py
+++ esdlvalidator/validation/functions/check_not_null.py
@@ -11,7 +11,8 @@ class ContainsNotNull(FunctionCheck):
             "Check if a value is set",
             [
                 ArgDefinition("property", "The name of the propery containing the value to check, leave propery out to check directly on input value", False),
-                ArgDefinition("counts_as_null", "Array of values which are seen as null values such as 0.0 for a double, 0 for int, 'NULL' for a string", False)
+                ArgDefinition("counts_as_null", "Array of values which are seen as null values such as 0.0 for a double, 0 for int, 'NULL' for a string", False),
+                ArgDefinition("resultMsgJSON", "Display output in JSON format", False)
             ]
         )
 
@@ -26,27 +27,43 @@ class ContainsNotNull(FunctionCheck):
         # Some esdl entity values have a default of undefined or none when not set
         include.extend(["undefined", "none"])
         value = self.value
+        msg = {"offending_asset": self.value.id}
 
         if hasProp:
             if not utils.has_attribute(value, prop):
-                return CheckResult(False, self.__create_message("property {0} not found".format(prop), value))
+                result = self.__create_message("property {0} not found".format(prop), value)
+                if 'resultMsgJSON' in self.args and self.args['resultMsgJSON']:
+                    msg["message"] = result
+                    return CheckResult(False, msg)
+                else:
+                    return CheckResult(False, result)
 
             value = utils.get_attribute(value, prop)
 
         if value is None:
-            return CheckResult(False)
+            result = self.__create_message("property {0} value is None".format(prop), value)
+            if 'resultMsgJSON' in self.args and self.args['resultMsgJSON']:
+                msg["message"] = result
+                return CheckResult(False, msg)
+            else:
+                return CheckResult(False, result)
 
-        return self.check_includes(include, value, self.value)
+        return self.check_includes(include, prop, value, self.value)
 
-    def check_includes(self, include, value, originalValue):
+    def check_includes(self, include, prop, value, originalValue):
+        msg = {"offending_asset": self.value.id}
         for includeValue in include:
             if str(includeValue).lower() == str(value).lower():
-                return CheckResult(False, self.__create_message("value equals {0}".format(includeValue), originalValue))
+                result = self.__create_message("{0} cannot be {1}".format(prop, includeValue), originalValue)
+                if 'resultMsgJSON' in self.args and self.args['resultMsgJSON']:
+                    msg["message"] = result
+                    return CheckResult(False, msg)
+                else:
+                    return CheckResult(False, result)
 
         return CheckResult(True)
 
     def __create_message(self, msg, value):
         if utils.has_attribute(value, "id"):
             msg += " for entity {0}".format(utils.get_attribute(value, "id"))
-
         return msg
