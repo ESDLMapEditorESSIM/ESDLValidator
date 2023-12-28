ad91b81ffbdf49e1f077a8fd3c728fd867db52d3
Femke Janssen <femke.janssen@tno.nl>
2023-12-28 16:22:24 +0100

    removed asset counting to prevent crash when outputting result

--- esdlvalidator/validation/functions/check_multi_cond_xor.py
+++ esdlvalidator/validation/functions/check_multi_cond_xor.py
@@ -49,7 +49,7 @@ class ContainsMultiConditionCheck(FunctionCheck):
                     return CheckResult(False, msg)
                 else:
                     return CheckResult(False, result)
-            print(str(utils.get_attribute(self.value, p)), p, v)
+            # print(str(utils.get_attribute(self.value, p)), p, v)
             if utils.get_attribute(self.value, p) == v:
                 fails += 1
 
